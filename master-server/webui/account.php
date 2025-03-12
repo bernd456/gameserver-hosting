14ae60bf6495aa8029e489df7c92735349a8ec63
bernd456 <djstyleshoot@live.de>
2025-03-12 08:41:05 +0100

    Update account.php

--- master-server/webui/account.php
+++ master-server/webui/account.php
@@ -4,6 +4,6 @@ include("includes/header.php");
 <div class="container">
     <h2>Mein Konto</h2>
     <p>Übersicht über Ihre gebuchten Gameserver, Rechnungen und Support-Tickets.</p>
-    <!-- Weitere Kunden-Funktionen hier einfügen -->
+    <!-- Hier können Kunden Details zu ihren Servern, den Wartungsstatus und Migrationsoptionen einsehen -->
 </div>
 <?php include("includes/footer.php"); ?>
