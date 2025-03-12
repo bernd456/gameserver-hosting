581d1e072abd7c701aed9d01aefc985054f672e7
bernd456 <djstyleshoot@live.de>
2025-03-12 08:20:05 +0100

    Create account.php

---
+++ master-server/webui/account.php
@@ -0,0 +1,9 @@
+<?php
+include("includes/header.php");
+?>
+<div class="container">
+    <h2>Mein Konto</h2>
+    <p>Übersicht über Ihre gebuchten Gameserver, Rechnungen und Support-Tickets.</p>
+    <!-- Weitere Kunden-Funktionen hier einfügen -->
+</div>
+<?php include("includes/footer.php"); ?>
