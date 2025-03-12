9169c5f81de2a8d0969347e09b8fce0a676aa38c
bernd456 <djstyleshoot@live.de>
2025-03-12 08:21:33 +0100

    Create terms.php

---
+++ master-server/webui/terms.php
@@ -0,0 +1,6 @@
+<?php include("includes/header.php"); ?>
+<div class="container">
+    <h2>Allgemeine Geschäftsbedingungen (AGB)</h2>
+    <p>Hier stehen die AGB für die Nutzung unseres Hosting-Systems...</p>
+</div>
+<?php include("includes/footer.php"); ?>
