616291372c5a3f49399f5ec58a930f3b3a0462a5
bernd456 <djstyleshoot@live.de>
2025-03-12 08:38:41 +0100

    Update footer.php

--- master-server/webui/includes/footer.php
+++ master-server/webui/includes/footer.php
@@ -1,1 +1,7 @@
+    <footer>
+        <p>&copy; 2025 Gameserver-Hosting System. Alle Rechte vorbehalten.</p>
+        <p><a href="terms.php">AGB</a> | <a href="privacy.php">Datenschutz</a></p>
+    </footer>
+</body>
+</html>
 
