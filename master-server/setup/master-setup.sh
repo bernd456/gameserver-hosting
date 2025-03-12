9959c8ac097ac7c60450601ede153668d38662f3
bernd456 <djstyleshoot@live.de>
2025-03-12 08:13:40 +0100

    Create master-setup.sh

---
+++ master-server/setup/master-setup.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+set -e
+
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+echo -e "${GREEN}Master-Server Installation startet...${NC}"
+
+apt update && apt upgrade -y
+apt install -y apache2 mariadb-server php php-cli php-mysql git unzip ufw
+
+ufw allow 22/tcp
+ufw allow 80/tcp
+ufw allow 443/tcp
+ufw allow 3306/tcp
+ufw --force enable
+
+echo -e "${GREEN}Bitte führen Sie 'mysql_secure_installation' aus.${NC}"
+mysql_secure_installation
+
+read -s -p "MySQL Root-Passwort: " MYSQL_ROOT_PASSWORD
+echo ""
+
+mysql -u root -p$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE gameserver_db;"
+mysql -u root -p$MYSQL_ROOT_PASSWORD -e "CREATE USER 'gameserver'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
+mysql -u root -p$MYSQL_ROOT_PASSWORD -e "GRANT ALL PRIVILEGES ON gameserver_db.* TO 'gameserver'@'localhost';"
+mysql -u root -p$MYSQL_ROOT_PASSWORD -e "FLUSH PRIVILEGES;"
+
+WEB_DIR="/var/www/html"
+rm -rf $WEB_DIR/*
+git clone https://github.com/DeinGitHubAccount/gameserver-hosting.git /tmp/gs_temp
+cp -r /tmp/gs_temp/master-server/webui/* $WEB_DIR/
+chown -R www-data:www-data $WEB_DIR
+chmod -R 755 $WEB_DIR
+
+mysql -u gameserver -p$MYSQL_ROOT_PASSWORD gameserver_db < $WEB_DIR/database.sql
+
+cat <<EOL > /etc/apache2/sites-available/gameserver.conf
+<VirtualHost *:80>
+    ServerAdmin admin@deinedomain.com
+    DocumentRoot $WEB_DIR
+    <Directory $WEB_DIR>
+        AllowOverride All
+        Require all granted
+    </Directory>
+    ErrorLog \${APACHE_LOG_DIR}/gameserver_error.log
+    CustomLog \${APACHE_LOG_DIR}/gameserver_access.log combined
+</VirtualHost>
+EOL
+
+a2ensite gameserver.conf
+a2enmod rewrite
+systemctl restart apache2
+
+rm -rf /tmp/gs_temp
+
+echo -e "${GREEN}Master-Server Installation abgeschlossen! Bitte prüfen Sie http://<server-ip> im Browser.${NC}"
