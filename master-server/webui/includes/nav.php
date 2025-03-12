a0ba327cb1a3833bed68161388d783a422db7128
bernd456 <djstyleshoot@live.de>
2025-03-12 08:39:02 +0100

    Update nav.php

--- master-server/webui/includes/nav.php
+++ master-server/webui/includes/nav.php
@@ -1,9 +1,9 @@
 <nav>
-    <ul>
-        <li><a href="index.php">Home</a></li>
-        <li><a href="store.php">Store</a></li>
-        <li><a href="account.php">Mein Konto</a></li>
-        <li><a href="tickets.php">Support</a></li>
-        <li><a href="admin.php">Admin</a></li>
-    </ul>
+  <ul>
+    <li><a href="index.php">Home</a></li>
+    <li><a href="products.php">Produkt Kategorie</a></li>
+    <li><a href="faq.php">FAQ</a></li>
+    <li><a href="serverstatus.php">Server Status</a></li>
+    <li><a href="account.php">Kundenkonto</a></li>
+  </ul>
 </nav>
