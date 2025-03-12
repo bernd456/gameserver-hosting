28db90f0959c5fcd0cc2856d0881793a53ca2848
bernd456 <djstyleshoot@live.de>
2025-03-12 08:15:44 +0100

    Create nav.php

---
+++ master-server/webui/includes/nav.php
@@ -0,0 +1,9 @@
+<nav>
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="store.php">Store</a></li>
+        <li><a href="account.php">Mein Konto</a></li>
+        <li><a href="tickets.php">Support</a></li>
+        <li><a href="admin.php">Admin</a></li>
+    </ul>
+</nav>
